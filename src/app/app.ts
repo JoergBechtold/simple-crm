463bf617ee24e634a689652f7e7543d5feaf6dc8
Jörg Bechtold <joergbechtold@gmx.net>
2025-08-01 13:32:38 +0200

    Sidenav implemented

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, MatToolbarModule,
+    MatIconModule,
+    MatButtonModule, MatSidenavModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
