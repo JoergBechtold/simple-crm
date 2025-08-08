bdd75e8688327f7c8e03d69c328ea2b5197a911c
Jörg Bechtold <joergbechtold@gmx.net>
2025-08-08 15:15:48 +0200

    routing to components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { Dashboard } from '../app/dashboard/dashboard';
+import { User } from '../app/user/user';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    { path: '', component: Dashboard },
+    { path: 'dashboard', component: Dashboard },
+    { path: 'user', component: User },
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,15 +1,16 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterModule } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 
 
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatToolbarModule,
+  imports: [RouterOutlet, RouterModule, MatToolbarModule,
     MatIconModule,
     MatButtonModule, MatSidenavModule],
   templateUrl: './app.html',
