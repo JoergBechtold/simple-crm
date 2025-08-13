47c3f53ece71a5d4a2a714d60036139a36daca02
Jörg Bechtold <joergbechtold@gmx.net>
2025-08-13 11:16:39 +0200

    Link Firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { RouterLink } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
+import { AsyncPipe } from '@angular/common';
+import { Firestore, collection, collectionData } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 
 @Component({
   selector: 'app-root',
-  imports: [MatToolbarModule, MatSidenavModule, MatIconModule, RouterOutlet, RouterLink, MatButtonModule],
+  imports: [MatToolbarModule,
+    MatSidenavModule,
+    MatIconModule,
+    RouterOutlet,
+    RouterLink,
+    MatButtonModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'simple-crm';
+  firestore: Firestore = inject(Firestore);
+  items$: Observable<any[]>;
+
+  constructor() {
+    const aCollection = collection(this.firestore, 'items')
+    this.items$ = collectionData(aCollection);
+  }
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: false,
+    firebase: {
+        apiKey: "AIzaSyB532vRMmQo7yWzY4mDcD1TsTa0D1tX32w",
+        authDomain: "simple-crm-451cc.firebaseapp.com",
+        projectId: "simple-crm-451cc",
+        storageBucket: "simple-crm-451cc.firebasestorage.app",
+        messagingSenderId: "1070772479809",
+        appId: "1:1070772479809:web:50bdcf7010868eafb6a3c0"
+    }
+};
