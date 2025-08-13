38a44d59975583c11cb21370a9cf3bdd9dab6690
Jörg Bechtold <joergbechtold@gmx.net>
2025-08-13 16:53:35 +0200

    firestore data subscribed

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -8,25 +8,30 @@ import { DialogAddUserComponent } from '../dialog-add-user/dialog-add-user.compo
 import { MatCardModule } from '@angular/material/card';
 import { Firestore, collection, collectionData } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
+import { CommonModule } from '@angular/common';
+import { User } from '../../models/user.class';
 
-
-export interface User {
-  // Annahme: Deine Benutzer haben ein 'name'-Feld und ein 'id'-Feld
-  id?: string;
-  name: string;
-  // ... weitere Felder
-}
-
-
+// Dein User-Interface
+// export interface User {
+//   birthDate: number;
+//   city: string;
+//   firstName: string;
+//   lastName: string;
+//   street: string;
+//   zipCode: number;
+// }
 
 @Component({
   standalone: true,
   selector: 'app-user',
-  imports: [MatButtonModule,
+  imports: [
+    MatButtonModule,
     MatIconModule,
     MatTooltipModule,
     MatDialogModule,
-    MatCardModule],
+    MatCardModule,
+    CommonModule
+  ],
   templateUrl: './user.component.html',
   styleUrl: './user.component.scss'
 })
@@ -34,20 +39,23 @@ export class UserComponent {
   readonly dialog = inject(MatDialog);
   readonly firestore = inject(Firestore);
 
-  users$!: Observable<User[]>;
+  // **Hier ist die entscheidende Änderung:**
+  // Das Observable wird direkt bei der Deklaration initialisiert.
+  // Hier ist die inject()-Funktion gültig und collectionData wird korrekt aufgerufen.
+  users$: Observable<User[]> = collectionData(collection(this.firestore, 'users')) as Observable<User[]>;
 
-  ngOnInit(): void {
-    const usersCollection = collection(this.firestore, 'users');
-    this.users$ = collectionData(usersCollection, { idField: 'id' }) as Observable<User[]>;
+  // Die ngOnInit()-Methode ist jetzt nicht mehr zwingend nötig,
+  // da das Observable bereits initialisiert ist. Du kannst es aber verwenden,
+  // um das Observable zu abonnieren, wenn du die Daten direkt in der Komponente
+  // benötigst. Die sauberste Lösung ist aber oft, die async-Pipe im Template zu verwenden.
 
-    this.users$.subscribe(changes => {
-      console.log('Received changes from DB', changes);
-    });
-  }
-
-
+  // ngOnInit(): void {
+  //   this.users$.subscribe(changes => {
+  //     console.log('Received changes from DB', changes);
+  //   });
+  // }
 
   openDialog() {
-    this.dialog.open(DialogAddUserComponent)
+    this.dialog.open(DialogAddUserComponent);
   }
 }
