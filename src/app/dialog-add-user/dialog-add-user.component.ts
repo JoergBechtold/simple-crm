2893b0d569fca5ee3229834a8070f64d7f4457e2
Jörg Bechtold <joergbechtold@gmx.net>
2025-08-13 09:29:47 +0200

    Date of birth input field created

--- src/app/dialog-add-user/dialog-add-user.component.ts
+++ src/app/dialog-add-user/dialog-add-user.component.ts
@@ -2,12 +2,14 @@ import { Component } from '@angular/core';
 import { MatDialogTitle, MatDialogContent, MatDialogActions } from '@angular/material/dialog';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 
 
 @Component({
   selector: 'app-dialog-add-user',
-  imports: [MatDialogTitle, MatDialogContent, FormsModule, MatDialogActions, MatButtonModule],
+  imports: [MatDialogTitle, MatDialogContent, FormsModule, MatDialogActions, MatButtonModule, MatInputModule, MatFormFieldModule],
   templateUrl: './dialog-add-user.component.html',
   styleUrl: './dialog-add-user.component.scss'
 })
