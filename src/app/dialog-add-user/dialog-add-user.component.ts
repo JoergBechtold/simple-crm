b82bd6580e6716452839b31698e74450b3f7236c
Jörg Bechtold <joergbechtold@gmx.net>
2025-08-13 09:39:20 +0200

    Datepicker created

--- src/app/dialog-add-user/dialog-add-user.component.ts
+++ src/app/dialog-add-user/dialog-add-user.component.ts
@@ -4,14 +4,23 @@ import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
-
-
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { provideNativeDateAdapter } from '@angular/material/core';
 
 @Component({
   selector: 'app-dialog-add-user',
-  imports: [MatDialogTitle, MatDialogContent, FormsModule, MatDialogActions, MatButtonModule, MatInputModule, MatFormFieldModule],
+  imports: [MatDialogTitle,
+    MatDialogContent,
+    FormsModule,
+    MatDialogActions,
+    MatButtonModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatDatepickerModule],
   templateUrl: './dialog-add-user.component.html',
-  styleUrl: './dialog-add-user.component.scss'
+  styleUrl: './dialog-add-user.component.scss',
+  providers: [provideNativeDateAdapter()],
+
 })
 export class DialogAddUserComponent {
 
